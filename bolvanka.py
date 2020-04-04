f8e6848615b4f2033cad4a2a47b4499a62fa6bf2
Afanasy Argunov <afanasy@argunov.com>
2020-04-05 00:43:18 +0900

    initial

---
+++ bolvanka.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Болванка для Пети"""
+
+import sys
+
+def main():
+    """Main"""
+    if len(sys.argv) > 1:
+        print(.98)
+    else:
+        print('Scan not found')
+
+if __name__ == "__main__":
+    main()
---
+++ savemedrecords.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Читает файл реестра медицинской истории и дописывает в другой файл"""
+
+import sys
+import xml.etree.ElementTree as et
+
+def main():
+    """Main"""
+
+if __name__ == "__main__":
+    main()
